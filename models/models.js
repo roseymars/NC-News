f9ce3895cf5a3ec5aec5a10af9e58b1fb10d7a0b
roseymars <roseymarissa@gmail.com>
2022-02-15 15:56:07 +0000

    minor refactor, removed promise reject

--- models/models.js
+++ models/models.js
@@ -8,12 +8,19 @@ exports.selectArticleById = (articleId) => {
   return db
     .query("SELECT * FROM articles WHERE article_id = $1;", [articleId])
     .then(({ rows }) => {
-      if (rows.length === 0) {
-        return Promise.reject({
-          status: 404,
-          msg: "PATH REQUESTED NOT FOUND",
-        });
-      }
+      // if (rows.length === 0) {
+      //   return Promise.reject({
+      //     status: 404,
+      //     msg: "PATH REQUESTED NOT FOUND",
+      //   });
+      // }
       return rows[0];
     });
 };
+
+exports.updateVotes = (articleId, votes) => {
+  const queryValues = []
+  let queryStr = 'INSERT INTO articles SET inc_votes= inc_votes + $2 WHERE article_id=$1;'
+  
+
+}
