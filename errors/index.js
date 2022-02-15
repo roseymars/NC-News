8b0530d2f0c39223633e443e9d3582e640da2934
roseymars <roseymarissa@gmail.com>
2022-02-15 14:06:39 +0000

    revert changes in handleCustomErrors based on PR comments

--- errors/index.js
+++ errors/index.js
@@ -1,17 +1,17 @@
 exports.handleCustomErrors = (err, req, res, next) => {
-    if (err.status === 400 && err.msg === "Invalid ID given") {
-        res.status(400).send({ msg: 'Invalid ID given' });
-      } else next(err);
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
 };
 
 exports.handlePsqlErrors = (err, req, res, next) => {
-  if (err.code === '22P02') {
-    res.status(400).send( { msg: 'Invalid input' } )
-  } 
-  next(err)
-}
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Invalid input" });
+  }
+  next(err);
+};
 // exports.handle500errors = (err, req, res, next) => {
-  
+
 // }
 
 // handle server errors
