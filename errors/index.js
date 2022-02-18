c81ddbb26355e28b2d22c3907bc778af7c80cc02
roseymars <roseymarissa@gmail.com>
2022-02-18 17:26:28 +0000

    remove console.log

--- errors/index.js
+++ errors/index.js
@@ -2,27 +2,25 @@ exports.handleCustomErrors = (err, req, res, next) => {
   if (err.status && err.msg) {
     // console.log(err)
     res.status(err.status).send({ msg: err.msg });
-  } 
+  }
   next(err);
 };
 
 exports.pathNotFound = (req, res) => {
-  res.status(404).send({ msg: "PATH REQUESTED NOT FOUND"})
-}
+  res.status(404).send({ msg: "PATH REQUESTED NOT FOUND" });
+};
 
 exports.handlePsqlErrors = (err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Invalid input" });
   } else if (err.code === "23502") {
-    res.status(400).send({ msg: "Invalid input" })
-  } else if (err.code === '23503') {
-    res.status(404).send({ msg: "PATH REQUESTED NOT FOUND" })
+    res.status(400).send({ msg: "Invalid input" });
+  } else if (err.code === "23503") {
+    res.status(404).send({ msg: "PATH REQUESTED NOT FOUND" });
   }
   next(err);
 };
 
 exports.handleServerErrors = (err, req, res, next) => {
-  console.log(err)
-  res.status(500).send( { msg: 'Internal Server Error'})
-}
-
+  res.status(500).send({ msg: "Internal Server Error" });
+};
